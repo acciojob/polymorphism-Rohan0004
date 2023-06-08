6f03668de4f476ce35751eb8cf661b6db023aa32
Rohan0004 <rohanraut0004@gmail.com>
2023-06-09 03:32:17 +0530

    Class Access

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,7 +1,7 @@
 package com.driver;
 
 public class Main {
-    static class Product{
+    public static class Product{
         public int product(int x, int y) {
             return x*y;
         }
@@ -17,8 +17,8 @@ public class Main {
         Product p=new Product();
 
         //method overloading
-        p.product(4,5);
-        p.product(4,5,9);
-        p.product(5.3,9.4);
+        System.out.println(p.product(4,5));
+        System.out.println(p.product(4,5,5));
+        System.out.println(p.product(5.3,9.4));
     }
 }
