19beb0ebf706ec387e634306a9f181763e68813e
Rohan0004 <rohanraut0004@gmail.com>
2023-06-08 14:15:34 +0530

    Method Overloading Basics

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,24 @@
 package com.driver;
 
 public class Main {
+    static class Product{
+        public int product(int x, int y) {
+            return x*y;
+        }
+        public int product(int x, int y,int z) {
+            return x*y*z;
+        }
+        public double product(double x, double y) {
+            return x*y;
+        }
+    }
 
+    public static void main(String[] args) {
+        Product p=new Product();
+
+        //method overloading
+        System.out.println(p.product(4,5));
+        System.out.println(p.product(4,5,9));
+        System.out.println(p.product(5.3,9.4));
+    }
 }
